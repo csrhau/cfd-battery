856cc819b45705b2ab82daebcf9cc4ce42005446
Stephen Roberts <stephenianroberts@gmail.com>
2015-09-30 17:53:57 +0100

    Added directional generator for alignment testing

--- input-gen/h5gen.py
+++ input-gen/h5gen.py
@@ -8,7 +8,7 @@ def process_arguments():
   parser.add_argument('-y', '--rows', type=int, default=31, help='Simulation domain height')
   parser.add_argument('-s', '--sigma', type=float, default=.25, help='Sigma')
   parser.add_argument('-n', '--nu', type=float, default=.05, help='Nu')
-  parser.add_argument('-g', '--generator', type=str, choices=['barbra', 'ones'], default='barbra')
+  parser.add_argument('-g', '--generator', type=str, choices=['barbra', 'ones', 'hotcorner'], default='barbra')
   parser.add_argument("-of", "--outfile", type=str, required=True, help="Path to data file to write to")
   return parser.parse_args()
 
@@ -23,12 +23,22 @@ def barbra_gen(rows, cols):
   data[.5/dy:1/dy+1,.5/dx:1/dx+1] = 2
   return data
 
+def hotcorner_gen(rows, cols):
+  data = np.zeros((rows, cols))
+  data[0:rows/3,0:cols/3] = 2
+  return data
+
+
 def main():
   args = process_arguments()
+
   if args.generator == 'barbra':
     data = barbra_gen(args.rows, args.cols)
   elif args.generator == 'ones':
     data = ones_gen(args.rows, args.cols)
+  elif args.generator == 'hotcorner':
+    data = hotcorner_gen(args.rows, args.cols)
+
   with h5py.File(args.outfile, 'w-') as of:
     ds = of.create_dataset('temperature', data=data) 
     of.attrs['generator'] = np.string_(args.generator)
