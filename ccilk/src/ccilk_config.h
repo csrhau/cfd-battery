90ad89f627df1a83595e0282cd13af4e15085150
Stephen Roberts <stephenianroberts@gmail.com>
2015-10-01 13:53:08 +0100

    got files opening

--- ccilk/src/ccilk_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef CCILK_CCILK_CONFIG_H
-#define CCILK_CCILK_CONFIG_H
-
-#define CCILK_DATASET "/temperature"
-
-#endif 
--- ccilk/src/main.h
+++ ccilk/src/main.h
@@ -1,7 +1,9 @@
 #ifndef CCILK_MAIN_H
 #define CCILK_MAIN_H
 
-void simulate(void);
+#define CCILK_DATASET "/temperature"
+
+void simulate(char *infile, char *prefix);
 void print_usage(void);
 
 #endif
