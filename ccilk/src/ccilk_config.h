17ad01fef0d26f6ecb14a8f99f8462f12558eb44
Stephen Roberts <stephenianroberts@gmail.com>
2015-10-01 12:49:08 +0100

    transitioned to cmake

---
+++ ccilk/src/ccilk_config.h
@@ -0,0 +1,6 @@
+#ifndef CCILK_CCILK_CONFIG_H
+#define CCILK_CCILK_CONFIG_H
+
+#define CCILK_DATASET "/temperature"
+
+#endif 
--- ccilk/src/main.c
+++ ccilk/src/main.c
@@ -6,9 +6,6 @@
 
 int main(int argc, char *argv[]) {
   hid_t file;
-
-
-
   printf("Hello, World!\n");
   return EXIT_SUCCESS;
 }
